7e4c762c06581d8b20ae9ed6058feac4625cac51
Victor <vdyachkov@nestline.com>
2014-08-18 14:11:45 +0400

    Improved version

--- Pod/Classes/Logger.h
+++ Pod/Classes/Logger.h
@@ -1,21 +1,25 @@
 //
 //  Logger.h
-//  AZNews
 //
 //  Created by Виктор Дьячков on 08.08.14.
 //  Copyright (c) 2014 Nestline. All rights reserved.
 //
 
 
-
-static BOOL enableLog = true;
-
 @interface Logger : NSObject 
-    
 
-+ (void) setDebugDictionary: (NSDictionary*) setParams;
+
++ (void) initWithEnableLoging: (BOOL) enableFlag
+              globalDebugDict: (NSDictionary*) debugDictionary
+                flurrySession: (NSString*) sessionSting
+                 flurryUserID: (NSString*) userString;
+
+
++ (void) logSuccessWithTitle: (NSString*) title message:(NSString*) message debugString: (NSString*) hiddenString alert:(BOOL) isAlert;
++ (void) logSuccessWithTitle: (NSString*) title message:(NSString*) message debugDict: (NSDictionary*) debugDict alert:(BOOL) isAlert;
 
 + (void) logErrorWithTitle: (NSString*) title message:(NSString*) message debugString: (NSString*) hiddenString alert:(BOOL) isAlert;
-+ (void) logSuccessWithTitle: (NSString*) title message:(NSString*) message debugString: (NSString*) hiddenString alert:(BOOL) isAlert;
++ (void) logErrorWithTitle: (NSString*) title message:(NSString*) message debugDict: (NSDictionary*) debugDict alert:(BOOL) isAlert;
+
 
 @end
--- Pod/Classes/Logger.m
+++ Pod/Classes/Logger.m
@@ -1,76 +1,110 @@
 //
 //  Logger.m
-//  AZNews
 //
 //  Created by Виктор Дьячков on 08.08.14.
 //  Copyright (c) 2014 Nestline. All rights reserved.
 //
+
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "Logger.h"
 #import "Flurry.h"
 
+
 @implementation Logger
 
 static NSDictionary* addParams = nil;
 
-+ (void) setDebugDictionary: (NSDictionary*) setParams
+static BOOL enableLog = false;
+static BOOL isInitialized = false;
+static NSString* initErrMsg = @"Please configure this module, call 'initWithEnableLoging...' before use logging";
+
+
++ (void) initWithEnableLoging: (BOOL) enableFlag
+              globalDebugDict: (NSDictionary*) debugDictionary
+                flurrySession: (NSString*) sessionSting
+                 flurryUserID: (NSString*) userString
 {
-    addParams = setParams;
+    addParams = debugDictionary;
+    enableLog = (enableFlag) ? enableFlag : false;
+    [Flurry setLogLevel:enableLog];
+    [Flurry setCrashReportingEnabled:YES];
+    if (sessionSting) [Flurry startSession:sessionSting];
+    if (userString) [Flurry setUserID:userString];
+    isInitialized = true;
 }
 
-+ (void) logErrorWithTitle: (NSString*) title message:(NSString*) message debugString: (NSString*) hiddenString alert:(BOOL) isAlert
-{
-    
-    if (isAlert) {
-        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-        [alert show];
-    }
-    
-    if (enableLog) {
-        NSMutableString* logString = [NSMutableString new];
-        [logString appendString:title];
-        if (message)  [logString appendFormat:@", %@",message];
-        if (hiddenString) [logString appendFormat:@", %@",hiddenString];
-        NSLog(@"### Error:   %@", logString);
-        
-        NSString* errTitle = [NSString stringWithFormat:@"# Error:   %@", title];
-        NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
-        if ([NSDate date]) [parameters setValue:[NSDate date] forKey:@"ts"];
-        if (addParams) [parameters addEntriesFromDictionary:addParams];
-        if (message) [parameters setValue:message forKey:@"message"];
-        if (hiddenString) [parameters setValue:hiddenString forKey:@"hiddenString"];
-        [Flurry logEvent:errTitle withParameters:parameters];
-    }
 
++ (void) logSuccessWithTitle: (NSString*) title message:(NSString*) message debugString: (NSString*) debugString alert:(BOOL) isAlert
+{
+    NSAssert(isInitialized, initErrMsg);
+    if (isAlert) [self alertWithTitle:title message:message];
+    [self setTitle:title message:message debugString:debugString debugDict:nil isSuccess:YES];
 }
 
-+ (void) logSuccessWithTitle: (NSString*) title message:(NSString*) message debugString: (NSString*) hiddenString alert:(BOOL) isAlert
++ (void) logSuccessWithTitle: (NSString*) title message:(NSString*) message debugDict: (NSDictionary*) debugDict alert:(BOOL) isAlert
 {
-    
-    if (isAlert) {
-        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-        [alert show];
-    }
-    
-    if (enableLog) {
-        NSMutableString* logString = [NSMutableString new];
-        [logString appendString:title];
-        if (message)  [logString appendFormat:@", %@",message];
-        if (hiddenString) [logString appendFormat:@", %@",hiddenString];
-        NSLog(@"### Success: %@", logString);
-        
-        NSString* errTitle = [NSString stringWithFormat:@"### Success: %@", title];
-        NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
-        if ([NSDate date]) [parameters setValue:[NSDate date] forKey:@"ts"];
-        if (addParams) [parameters addEntriesFromDictionary:addParams];
-        if (message) [parameters setValue:message forKey:@"message"];
-        if (hiddenString) [parameters setValue:hiddenString forKey:@"hiddenString"];
-        [Flurry logEvent:errTitle withParameters:parameters];
-    }
-    
+    NSAssert(isInitialized, initErrMsg);
+    if (isAlert) [self alertWithTitle:title message:message];
+    [self setTitle:title message:message debugString:nil debugDict:debugDict isSuccess:YES];
 }
 
+
++ (void) logErrorWithTitle: (NSString*) title message:(NSString*) message debugString: (NSString*) debugString alert:(BOOL) isAlert
+{
+    NSAssert(isInitialized, initErrMsg);
+    if (isAlert) [self alertWithTitle:title message:message];
+    [self setTitle:title message:message debugString:debugString debugDict:nil isSuccess:NO];
+}
+
++ (void) logErrorWithTitle: (NSString*) title message:(NSString*) message debugDict: (NSDictionary*) debugDict alert:(BOOL) isAlert
+{
+    NSAssert(isInitialized, initErrMsg);
+    if (isAlert) [self alertWithTitle:title message:message];
+    [self setTitle:title message:message debugString:nil debugDict:debugDict isSuccess:NO];
+}
+
+
++ (void) alertWithTitle: (NSString*) title message: (NSString*) message
+{
+    UIAlertView* alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+    [alert show];
+}
+
++ (void) setTitle: (NSString*) title
+          message: (NSString*) message
+      debugString: (NSString*) debugString
+        debugDict: (NSDictionary*) debugDict
+        isSuccess: (BOOL) success
+{
+    if (!enableLog) return;
+    
+    NSString* resultSting = (success) ? @"### Success: " : @"### Error:   ";
+
+    NSMutableString* logString = [NSMutableString new];
+    if (title) [logString appendString:title];
+    if (message)  [logString appendFormat:@", %@",message];
+    
+    NSString* infoTitle = [NSString stringWithFormat:@"%@%@", resultSting, title];
+    NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
+    if ([NSDate date]) [parameters setValue:[NSDate date] forKey:@"ts"];
+    if (addParams) [parameters addEntriesFromDictionary:addParams];
+    if (message) [parameters setValue:message forKey:@"message"];
+    
+    if (debugString) {
+        [logString appendFormat:@", %@",debugString];
+        [parameters setValue:debugString forKey:@"hiddenString"];
+    }
+    
+    if (debugDict) {
+        [logString appendFormat:@", %@",debugDict];
+        [parameters addEntriesFromDictionary:debugDict];
+    }
+    
+    NSLog(@"%@%@", resultSting, logString);
+    [Flurry logEvent:infoTitle withParameters:parameters];
+}
 
 
 @end
